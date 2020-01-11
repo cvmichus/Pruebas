bd4c5a2955adaee3afb449844b15de0ac464ca89
Carlos Valentin Michaus Barcenas <33094136+cvmichus@users.noreply.github.com>
2020-01-10 18:18:27 -0600

    10_01_2020
    10_01_2020

--- views/java_view.php
+++ views/java_view.php
@@ -3,71 +3,77 @@
   <head>
      <meta charset="UTF-8">
         <title>Ejemplo MVC con PHP</title>
-    
      <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-          
-
   </head>
        
         <style>
       
         </style>
-  <body >
-
+  <body>
 
 <div >
+<div >
+
+  <div id="root">
 <div class="container">
+  <p> <div id="Sucursal"></div></p>
+</div>
+</div>
   
   <div id="tabla"  class="table-responsive">
 
-
-<div id="root">
-<div class="container"></div>
-</div>
-
-
- <p>Id_Sucursal: <span id="IdSucursal"></span></p>
-  <p>Sucursal <span id="Sucursal"></span></p>
+ 
    
-
  <script>
+const app = document.getElementById('root');
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
 
-  const app = document.getElementById('root')
-  const container = document.createElement('div')
-  container.setAttribute('class', 'container')
-
-  app.appendChild(container)
+app.appendChild(container)
 
   var request = new XMLHttpRequest()
   request.open('GET', 'http://localhost:3333/api/v1/suc', true)
   request.onload = function() {
        var data = JSON.parse(this.response)
+       $('#Sucursal').html('<strong> Cargando info... </strong>');
         if (request.status >= 200 && request.status < 400) {
-                console.log("ok"+response);
-            
+                //console.log("ok"+response);
+                data.forEach(movie => {
+                $('#Sucursal').html('<strong> SUCURSALES</strong>');
+
+                  const card = document.createElement('div');
+                  card.setAttribute('class', 'card');
+
+                  const h1 = document.createElement('td');
+                  h1.textContent = movie.IdSucursal;
+
+                const p = document.createElement('td')
+                p.textContent = movie.Sucursal;
+
+
+                container.appendChild(card);
+                card.appendChild(h1);
+                card.appendChild(p);
+
+                   
+                })
         }
         else{
-                console.log("bad"+response);
-
+                //console.log("bad"+response);
+                alert('Disculpe, existió un problema');
         }
-
   }
 
   request.send()
 
       
-
 </script>
-
-
- 
-      
 
 </div>
 </div>
 
  <div class="col-lg-12 text-center">
+
                     <hr/>
                 
                      <a href="../index.php"> <i class="fa fa-arrow-circle-left"></i> Volver a la página principal</a>
